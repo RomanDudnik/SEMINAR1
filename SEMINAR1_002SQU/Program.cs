1a18b3723edac1e6849374d34762bf5db2ed3002
«Roman <research777@mail.ru>
2022-11-11 19:27:03 +0300

    SEMINAR1_002SQU

--- SEMINAR1_002SQU/Program.cs
+++ SEMINAR1_002SQU/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Написать программу, которая на вход принимает число и выдаёт его квадрат
+
 Console.WriteLine("Введите число, квадрат которого нужно найти: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int result = number * number;
+int number = Convert.ToInt32(Console.ReadLine());   // int - целое число, преобразовать из числа в строку - Convert.ToInt32(32 кол-во цифр в числе)
+int result = number * number;   
 Console.WriteLine($"Квадрат {number} равен {result}");
