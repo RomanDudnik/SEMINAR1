ccf73f00856445af5122e92a9e46937831d16979
«Roman <research777@mail.ru>
2022-11-11 11:06:46 +0300

    SEMINAR1_003SQU1

---
+++ SEMINAR1_003SQU1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, которая на вход принимает два числа и проверяет
+//является ли первое число квадратом второго.
+//a = 25, b = 5 -> да
+//a = 2, b = 10 -> нет
+//a = 9, b = -3 -> да
+//a = -3, b = 9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine($"число {number1} является квадратом {number2}");
+}
+
+else
+{
+    Console.WriteLine($"число {number1} не является квадратом {number2}");
+}
