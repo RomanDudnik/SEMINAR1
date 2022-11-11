ada9661c246052d7199e5818cb35d9ab0a7e5608
«Roman <research777@mail.ru>
2022-11-11 14:52:07 +0300

    SEMINAR1_001GoodMorning

--- SEMINAR1_001GoodMorning/Program.cs
+++ SEMINAR1_001GoodMorning/Program.cs
@@ -4,4 +4,4 @@ int number = 11;
 
 Console.WriteLine("Доброе утро! Сегодня " + number + " ноября ");
 
-Console.WriteLine($"Доброе утро! Сегодня {numbers} ноября ");
+Console.WriteLine($"Доброе утро! Сегодня {number} ноября ");
